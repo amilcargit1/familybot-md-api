d4ea821ddaf3b0e82b5a6795f0dc72cc33422144
amilcargit1 <amilcargit@gmail.com>
2026-08-22 07:15:36 -0500

    Actualizar auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -111,7 +111,7 @@ router.post('/update-profile', async (req, res) => {
 
 // ============== ESTADÍSTICAS GLOBALES (para la portada) ==============
 router.get('/stats', (req, res) => {
-    res.json({ status: true, users: db.countUsers(), endpoints: 6 });
+    res.json({ status: true, users: db.countUsers(), endpoints: 9 });
 });
 
 // ============== CANJEAR CÓDIGO ==============
